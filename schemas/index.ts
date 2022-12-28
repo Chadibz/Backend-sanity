1d055dc1e4a421dc502dd9974fa273825affb985
Chadibz <91723882+Chadibz@users.noreply.github.com>
2022-12-28 21:57:11 +0100

    skills

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,4 +1,5 @@
 import testimonial from './testimonial'
 import abouts from './abouts'
+import skills from './skills'
 
-export const schemaTypes = [testimonial,abouts]
+export const schemaTypes = [testimonial,abouts,skills]
