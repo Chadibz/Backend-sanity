1802aadea5aa9c74eb89cffad70af4acf3d36fe3
Chadibz <91723882+Chadibz@users.noreply.github.com>
2022-12-29 21:35:58 +0100

    some changes

--- schemas/index.ts
+++ schemas/index.ts
@@ -2,5 +2,6 @@ import testimonial from './testimonial'
 import abouts from './abouts'
 import skills from './skills'
 import contact from './contact'
+import expereinces from './experiences'
 
-export const schemaTypes = [testimonial,abouts,skills,contact]
+export const schemaTypes = [testimonial,abouts,skills,contact,expereinces]
